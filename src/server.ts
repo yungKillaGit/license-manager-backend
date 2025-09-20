bf15d03626126389ae8848208f32c8af4a5693eb
Artur Iskhakov <artur.iskhakov@waveaccess.global>
2025-09-20 13:43:50 +0500

    style: apply ESLint fixes

--- src/server.ts
+++ src/server.ts
@@ -1,22 +1,21 @@
 import autoLoad from '@fastify/autoload'
-import { fileURLToPath } from 'node:url'
-import { dirname, join } from 'node:path'
 import fastify from 'fastify'
+import { dirname, join } from 'node:path'
+import { fileURLToPath } from 'node:url'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = dirname(__filename)
 
 const app = fastify({
-  logger: true
+  logger: true,
 })
 
 app.register(autoLoad, {
-  dir: join(__dirname, 'plugins')
+  dir: join(__dirname, 'plugins'),
 })
 
 app.register(autoLoad, {
-  dir: join(__dirname, 'routes')
+  dir: join(__dirname, 'routes'),
 })
 
 app.listen({ port: 3000 })
-
